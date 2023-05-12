e649e60651b4d51b179c5efef1570a6514214a40
whichFahim <31901239+whichfahim@users.noreply.github.com>
2023-05-12 00:19:03 -0500

    Initialize main
    Pseudocode for main

---
+++ example project/Assets/Scripts/Main.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Main : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //initialize the first scene of the game
+        //hand control over to the scene
+        //scene will load the assets
+    }
+
+    // Update is called once per frame
+    /*
+    void Update()
+    {
+        
+    }
+    */
+}
